44f705db5e96e8f008f75d805d2d75f24ed02ce8
Zachary Foster <mech@mechbook-pro.local>
2020-08-10 13:11:53 -0700

    Working expressions to grab phone numbers and emails and adding them to a list will work on adding to clipboard on the next session

--- ph_em_extract.py
+++ ph_em_extract.py
@@ -2,13 +2,28 @@
 import re
 import pyperclip # Can put stuff into the clipboard
 
-# TODO grab all email addresses
+contact_list = []
+# TODO grab all email addresses (For now just create the proper expression that will grab what we need)
 email_regex = re.compile(r'\w*@\w*\.\w*')
 mo = email_regex.findall('zfoster777@gmail.com k8hoxie@gdfg.net dogcatmouse@pie.com beepbopbooe@sdfsdef.edu 12334234@34234.gov DOGBEARMOUdse@gmail.com')
-print(mo)
-# TODO grab all phone numbers
-#phone_regex = re.compile(r'''(
-    
+for i in mo:
+    contact_list.append(i)
+
+
+# TODO grab all phone numbers (For now just create the proper expression that will grab what we need)
+phone_regex = re.compile(r'''(
+     (\d{3}|\(\d{3}\))? # First 3 digits (area code)
+     (-|\s*|\.)? # Seperator (if any)
+     (\d{3}) # Next set of 3 digits
+     (-|\s*|\.) # Seperator (if any)
+     (\d{4,7})
+     )''', re.VERBOSE)
+moph = phone_regex.findall('505-967-8766, (242) 566 4567, 456 789 3455, 456-789-1010, 5053455678')
+index = len(moph)
+
+for i in moph:
+    contact_list.append(i[0])
+print(contact_list)
 
 # TODO Put all of them into the clipboard
 # TODO Paste them off of the clipboard
